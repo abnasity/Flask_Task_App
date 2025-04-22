888508997a7eddd972e5d1cfe073a7a6a92a4929
Kipngeno Abednego <abnasgit@gmail.com>
2025-04-22 23:28:38 +0300

    added new functions and imported tasks

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,27 @@
 from flask import render_template
 from app import app
 from app.models import User
+from app.models import Task
+
 
 
 @app.route('/')
 def home():
     users = User.query.all()
-    return render_template('index.html', users=users, title='Home')
+    user_count = User.query.count()
+    tasks = Task.query.all()
+    task_count = Task.query.count()
+   
+    
+    return render_template('index.html', user_count=user_count,  task_count=task_count, users=users, title='Home')
+
 
 @app.route('/tasks')
 def tasks():
-    return render_template('tasks.html', title='Tasks')
+    tasks = Task.query.all()
+    return render_template('tasks.html', tasks=tasks, title='Tasks')
+
+
 
 @app.route('/users')
 def users():
