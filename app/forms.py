57674f02338dab7db7ff9739ffda299484180afb
Kipngeno Abednego <abnasgit@gmail.com>
2025-04-23 23:10:16 +0300

    added forms and applied various forms fields

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, BooleanField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class TaskForm(FlaskForm):
+     title = StringField('Task Title', validators=[DataRequired()])
+     description = TextAreaField('Task Description', validators=[DataRequired()])
+     completed = BooleanField('Completed', validators=[DataRequired()])
+     submit = SubmitField('Submit', validators=[DataRequired()])
+     
+class UserForm(FlaskForm):
+     
+     username = StringField('User', validators=[DataRequired()])
+     first_name = StringField('First Name',  validators=[DataRequired()])
+     last_name = StringField('Last Name',  validators=[DataRequired()])
+     password = PasswordField('Password',  validators=[DataRequired()])
+ 
+     submit = SubmitField('Register')
